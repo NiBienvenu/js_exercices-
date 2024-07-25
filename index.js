32f5c13e03d010b8326333ff331a22370e4b18ce
“NiBienvenu” <nicibienvenu@gmail.com>
2024-07-25 19:35:25 +0200

    commentaire du blog

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 console.log('Running')
 
 //declaration des variables
-let a = 5;
+let a = -5;
 let b = 10;
 let c = 0;
 var d = "abcdef"
-const f = false
+const f = -8
+
+//affichage des variables
+console.log('a vaut :', a);
+console.log('b vaut :', b);
+console.log('d vaut :', d);
+
+//convert variables
+/* a = a.toString();  //commentaire du blog
+    b = b.toString();*/
+
+// conditions des variables
+if(a < b){
+    console.log('a est plus petit que b');
+}else if(a > b){
+    console.log('a est plus grand que b');
+}else{
+    console.log('a est égal à b');
+}
 
 if(f){
     c = a + b;
