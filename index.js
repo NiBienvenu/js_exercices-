06fc6beda9326c0e061605964f70e31d38d7d196
“NiBienvenu” <nicibienvenu@gmail.com>
2024-07-25 19:28:18 +0200

    condition

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log('Running')
+
+//declaration des variables
+let a = 5;
+let b = 10;
+let c = 0;
+var d = "abcdef"
+const f = false
+
+if(f){
+    c = a + b;
+    console.log('c vaut :', c);
+}else{
+    console.log('c n\'est pas calculé car la variable f est false');
+}
