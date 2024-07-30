e4168d9e18a00eac3f011cf9e9bc0e2b965680e4
“NiBienvenu” <nicibienvenu@gmail.com>
2024-07-30 22:15:03 +0200

    mathFonction

--- mesfonction.js
+++ mesfonction.js
@@ -19,8 +19,17 @@ function majeur(a){
     }
 }
 
+//Math fonction
+
+function mathFonction() {
+    let a = Math.cos(360)
+    let b = Math.sin(360)
+    console.log(a +" "+b) /// r
+}
+
 var t=addition(12,6)
 console.log('addition result :',t)
 var w=soustraction(12,8)
 console.log('soustraction result :',w)
 console.log('personne a 20 ans est :',majeur(20))
+mathFonction()
