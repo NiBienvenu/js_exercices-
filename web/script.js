73280af7790bbf8a2e4705c59a432c42c3b7ecc5
“NiBienvenu” <nicibienvenu@gmail.com>
2024-07-31 19:23:31 +0200

    onclick

---
+++ web/script.js
@@ -0,0 +1,12 @@
+// alert('HELLO WORD')
+
+var a = document.getElementById('hello').innerHTML; // 
+console.log(a)
+var b = document.getElementById('yves').innerHTML='Bonsoir Yves';
+console.log(b)
+
+function onclickButton(){
+    let a =  document.getElementById('nom').value 
+    document.getElementById('hello').innerHTML = a; // 
+    document.getElementById('nom').value='amahoro';
+}
