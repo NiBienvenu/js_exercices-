b29b2c91a3e24e1da22193824836ac4fe096eb82
“NiBienvenu” <nicibienvenu@gmail.com>
2024-07-31 20:10:57 +0200

    chage size

--- web/script.js
+++ web/script.js
@@ -15,4 +15,29 @@ function changement(){
     let a = document.getElementById('nom').value
     console.log(a)
     document.getElementById('hello').innerHTML=a;
+    changecolor()
+}
+
+function changecolor(){
+
+    var a = Math.random().toFixed(2)*100;
+    var b = Math.random().toFixed(2)*100;
+    var c = Math.random().toFixed(2)*100;
+    var d = Math.random().toFixed(2)*100;
+    //  while (a>255 || b>255 || c>255 ) {
+    //     changecolor()
+    //  }
+    // document.getElementById('hello').style.color = 'rgb('+(a)+','+b+','+c+')';  //
+    document.getElementById('hello').style.color = 'rgb('+(a+d)+','+(b+d)+','+(c+d)+')';  //
+    changeTextSize()
+
+    console.log(a)  
+    
+}
+
+function changeTextSize() {
+    let a = document.getElementById('nom').value
+    document.getElementById('yves').style.fontSize = ''+a+'px';
+    // document.getElementById('yves').style.display = 'none'
+
 }
